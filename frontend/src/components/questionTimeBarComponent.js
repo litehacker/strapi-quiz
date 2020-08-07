be45d1cb3308f849255138dff5c694ba5d9a1ba4
Giorgi Gvimradze <giorgigvimradze@Giorgis-MacBook-Pro.local>
2020-08-08 01:41:50 +0400

    minor update

---
+++ frontend/src/components/questionTimeBarComponent.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import { HorizontalBar } from 'react-chartjs-2';
+
+function TimeBar() {
+
+
+  const [timeData,setTimeData] = useState({});
+
+
+
+  useEffect(()=>{
+    const chart = () => {
+      setTimeData({
+        datasets:
+        [
+          {
+            data:[12],
+            backgroundColor:['#61dafb'],
+          },
+        ]
+      });
+    }
+    chart();
+  },[])
+
+  return (
+        <div className="col-12">
+            <HorizontalBar data={timeData} width={100} height={150} options={{ tooltips:{enabled:false}, maintainAspectRatio: false }}/>
+        </div>
+  );
+}
+// systemctl stop nginx
+// /opt/letsencrypt/certbot-auto renew
+// systemctl start nginx
+export default TimeBar;
--- frontend/src/components/rightBarComponent.js
+++ frontend/src/components/rightBarComponent.js
@@ -3,63 +3,64 @@ import { Doughnut } from 'react-chartjs-2';
 import '../css/timerComponent.css'
 
 const RightBar = () =>{
-  const sinav_suresi = 10;
+
+  const sinav_suresi = 90;
   //left time calculator
   let finish = new Date();
   finish.setSeconds(finish.getSeconds() + sinav_suresi);
-  const [endTime,setEndTime] = useState(finish);
+  const [endTime] = useState(finish);
 
   const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
     const difference = +endTime - +new Date();
-    let timeLeft = {};
-
-
-    if (difference > 0) {
-      timeLeft = {
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      };
+    if (difference>0)
+      return difference;
+    else {
+      return 0
     }
-    return timeLeft;
   }
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
+  const checkTimeFunction = () => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft);
+    },1000);
+  };
+
+// To-Do !! add dependency array
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-      console.log(timeLeft);
-    }, 1000);
+    checkTimeFunction()
   });
 
   //chart
   const [chartData,setChartData] = useState({});
 
-  const chart = () => {
-    setChartData({
-      datasets:
-      [
-        {
-          data:[timeLeft.seconds,0],
-          backgroundColor:['#61dafb'],
-        },
-      ]
-    });
-  }
+  useEffect( () => {if(timeLeft){
+    const chart = () => {
+      if(timeLeft){
+        setChartData({
+          datasets:
+          [
+            {
+              data:[timeLeft, sinav_suresi*1000 - timeLeft],
+              backgroundColor:['#61dafb'],
+            },
+          ]
+        });
+      }
+    }
+    chart(timeLeft);
 
-  useEffect(()=>{
-    chart()
-  },[]);
+  }}, [timeLeft] );
 
   return(
     <div className="col-3">
       <div className="timer">
         <div>
-          <span>{timeLeft.seconds ? `${timeLeft.hours}` + ':' + `${timeLeft.minutes}` + ':' + `${timeLeft.seconds}` : 'Süreniz Dolmuştur'}</span>
+          <span>{timeLeft ? Math.floor((timeLeft / (3600000)) % 24) + ':' + Math.floor((timeLeft / 60000) % 60) + ':' + Math.floor((timeLeft / 1000) % 60) : 'Süreniz Dolmuştur'}</span>
         </div>
         <Doughnut data={chartData}  options={{height:'200 px',tooltips:{enabled:false},maintainAspectRatio: true, cutoutPercentage:80, animation:{animateRotate:true}}}/>
+
       </div>
     </div>
   );
