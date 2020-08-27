2cfaed32648447d51aae8a866eb3cbcb1f6b9d6c
Giorgi Gvimradze <giorgigvimradze@Giorgis-MacBook-Pro.local>
2020-08-27 22:45:41 +0400

    update 404

--- frontend/src/pages/_404Page.js
+++ frontend/src/pages/_404Page.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import image404 from '../img/2634442.jpg';
+import image404 from '../img/2452502.jpg';
 
 function _404Component() {
   return (
     <div className="col-12 ">
-          <div className="col-3">
+          <div className="col-2">
            
           </div>
-          <div className="col-6" >
-          <img style={{ width: '100vh'}} src={image404}/>
+          <div className="col-8" >
+          <img style={{ width: '100%', height: 'auto'}} src={image404}/>
           <a style={{'fontSize': '5px'}} href="http://www.freepik.com">Designed by pikisuperstar / Freepik</a>  
           </div>
-          <div className="col-3">
+          <div className="col-2">
             
           </div>
         </div>
