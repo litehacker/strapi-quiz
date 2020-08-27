b773a3fcd10a5bc6c0577d1250503c4a0c97b17f
Giorgi Gvimradze <giorgigvimradze@Giorgis-MacBook-Pro.local>
2020-08-27 22:40:59 +0400

    spinner and 404 page update

--- frontend/src/components/pageComponent.js
+++ frontend/src/components/pageComponent.js
@@ -1,27 +1,33 @@
 import React, { lazy, Suspense } from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Header from "./topMenuComponent";
+import Spinner from 'react-bootstrap/Spinner';
+
 // pages
 const Question = lazy(() => import("../pages/quizPage.js"));
 const Login = lazy(() => import("./login.component.js"));
 const Register = lazy(() => import("./signup.component.js"));
 const LandingPage = lazy(() => import("./landingPageComponent.js"));
+const _404Component = lazy(() => import("../pages/_404Page.js"));
 
 const Page = () => {
   return (
-    
-    
-      <Router>
+    <Router>
       <Header/>
+      <Suspense fallback={
+        <Spinner animation="border" role="status">
+            <span className="sr-only">Yükleniyor...</span>
+        </Spinner>}>
         <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
             <Route path="/" exact component={LandingPage}/>
             <Route path="/quiz" component={Question}/>
             <Route path="/login" component={Login}/>
             <Route path="/register" component={Register}/>
-          </Suspense>
+            <Route path='*' exact={true} component={_404Component} />
+
         </Switch>
-      </Router>
+      </Suspense>
+    </Router>
   );
 }
 export default Page;
---
+++ frontend/src/pages/_404Page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import image404 from '../img/2634442.jpg';
+
+function _404Component() {
+  return (
+    <div className="col-12 ">
+          <div className="col-3">
+           
+          </div>
+          <div className="col-6" >
+          <img style={{ width: '100vh'}} src={image404}/>
+          <a style={{'fontSize': '5px'}} href="http://www.freepik.com">Designed by pikisuperstar / Freepik</a>  
+          </div>
+          <div className="col-3">
+            
+          </div>
+        </div>
+  );
+}
+
+export default _404Component;
