18408b90a208609997dcad9f0eeb864b84644f12
Giorgi Gvimradze <natceo@gmail.com>
2020-09-10 02:01:21 +0400

    changes of registration page

--- frontend/src/components/signup.component.js
+++ frontend/src/components/signup.component.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'  
+import {Link} from 'react-router-dom'
 import axios from 'axios';  
+import image from '../img/register.svg'
 
 function Regster(props) {  
   const [data, setdata] = useState({username:'', email: '', password: '',  })  
@@ -21,35 +23,48 @@ function Regster(props) {
     setdata({ ...data, [e.target.name]: e.target.value });  
   }  
   return (  
-    <div class="container">  
-      <div class="card o-hidden border-0 shadow-lg my-5" style={{ "marginTop": "5rem!important;" }}>  
-        <div class="card-body p-0">  
-          <div class="row">  
-            <div class="col-lg-12">  
-              <div class="p-5">  
-                <div class="text-center">  
-                  <h1 class="h4 text-gray-900 mb-4">Create a New User</h1>  
+    <div className="container">  
+      <div className="card o-hidden border-0 shadow-lg my-5" style={{ "marginTop": "5rem!important" }}>  
+        <div className="card-body p-0">  
+          <div className="row">  
+            <div className="col-lg-5">  
+              <div className="p-5">  
+                <div className="text-center">  
+                  <h1 className="h4 text-gray-900 mb-4">Create a New User</h1>  
                 </div>  
-                <form onSubmit={Registration} class="user">  
-                  <div class="form-group row">  
-                    <div class="col-sm-6 mb-3 mb-sm-0">  
-                      <input type="text" name="username" onChange={onChange} value={data.username} class="form-control"  placeholder="username" />  
-                    </div>  
-                    <div class="col-sm-6 mb-3 mb-sm-0">  
-                      <input type="text" name="email" onChange={onChange} value={data.email} class="form-control"  placeholder="Email" />  
-                    </div>  
-                    <div class="col-sm-6">  
-                      <input type="Password" name="password" onChange={onChange} value={data.password} class="form-control" placeholder="Password" />  
-                    </div>  
-                  </div>  
-                  <button type="submit" class="btn btn-primary  btn-block">  
+                <form onSubmit={Registration} className="user"> 
+                    <div className="form-group">
+                        <label>Kullanıcı Adı</label>
+                        <input type="text" name="username" onChange={onChange} value={data.username} className="form-control" placeholder="kullanıcı adınızı yazınız" />
+                    </div> 
+                    <div className="form-group">
+                        <label>Email Adresiniz</label>
+                        <input type="text" name="email" onChange={onChange} value={data.email} className="form-control" placeholder="ornek@email.com" />
+                    </div> 
+                    <div className="form-group">
+                        <label>Şifrenizi Oluşturun</label>
+                        <input type="password" name="password" onChange={onChange} value={data.password} className="form-control" />
+                    </div> 
+                  
+                  <button type="submit" className="btn btn-primary  btn-block">  
                     Create User  
-                </button>  
-                  <hr />  
+                  </button>  
+                    
                 </form>  
                 <hr />  
+                <div className="d-flex">
+                        <p className="forgot-password mr-auto">
+                            Üyeliğiniz var mı? <Link to="/login">Giriş Yapın</Link>
+                        </p>
+                        <p className="forgot-password">
+                            Şifremi <Link to="şifremi-unuttum">unuttum</Link>
+                        </p>
+                    </div>
               </div>  
             </div>  
+            <div className="col-lg-5 align-self-center px-5">
+                <img src={image} alt="register" style={{'max-width':'100%'}}/>
+            </div>
           </div>  
         </div>  
       </div>  
