6dbf6778788592a09dd8aae81c8f2c3728dcd5ba
Giorgi Gvimradze <natceo@gmail.com>
2020-11-16 02:33:20 +0400

    exams page

--- frontend/src/components/exams.js
+++ frontend/src/components/exams.js
@@ -1,94 +1,63 @@
 import React , {useState,useEffect} from "react";
-import {Link} from 'react-router-dom';
 import axios from 'axios';  
-import image from '../img/register.svg'
-import { useHistory } from "react-router-dom";
-/* App.js */
-
+import { useHistory } from 'react-router-dom';
 import Button from "react-bootstrap/Button"
-
-
 import '../css/questionComponent.css';
 import '../css/questionComponent-radio.css';
-var tmp
-function Exams() {
-    const [exams,setExam]=useState({id:0,title:"examText",examtime:0,examprice:0,sinavtarihi:Date,soru_sayisi:0});
-    const apiUrl = "https://sinavhukuk.com/api/exams";  
-    const [errorMessage, setErrorMessage] = useState('');
-    const [success, setSuccess] = useState('');
 
-  
+
+function Exams() {
+    const [exams,setExam]=useState();
+    const apiUrl = "https://sinavhukuk.com/api/exams";  
+
+    const history = useHistory();
+    const routeChange = () =>{ 
+      let path = `/deneme-sinavi`; 
+      history.push(path);
+    }
   useEffect( () => {
     try {
       const fetchData = async () => {
-        const response = await axios(
-          apiUrl,
+        const response = await axios(apiUrl);
+        console.log(response.data)
+        const listItems = response.data.map((exam) =>
+          <div key={exam.id} className="card p-2">
+            <span className="badge badge-secondary">Süre: {exam.exam_time} dakika</span>
+            <div className="exam ">
+              {exam.title ? exam.title : ''}: Soru sayısı:{exam.soru_sayisi ? exam.soru_sayisi : ''}
+            </div>
+            <div >
+              <div className="col-6 col-md-4">
+                <Button variant="outline-primary" size="md" onClick={routeChange}>
+                  Sınavı Al
+                </Button>
+              </div>
+            </div>
+          </div>
         );
-      setExam({ exams: response.data });
-        }
-        fetchData();
+        setExam(listItems)
+      }
+      fetchData();
      }catch (error) {
-      setExam({ error });
+       //console.log(error)
     }
   },[]);
-  tmp=1;
-  console.log(exams)
-  
-  
-  //setItems1({id:questionID,cvp:tmp})
-
-  const next  = () => { 
-    console.log('next')
-    
-   // setIsLoading(true)
-   // setExamID(prev => prev + 1)
-    
-    //socket.emit('getQuestionNext', questionID);
-    //socket.emit('userAnswer', answer);
-  }
-  const submit  = () => { 
-
-    //setIsLoading(true)
-    
-  }
-
-
-
-  const previous = () =>{
-   
-//    setIsLoading(true)
-   // setExamID(prev => prev - 1 ? prev - 1: prev)
-  }
-  
+/*
+created_at: "2020-09-14T13:37:19.410Z"
+exam_time: 9
+id: 1
+price: 40
+questions: []
+sinav_tarihi: "2021-02-09T22:30:00.000Z"
+soru_sayisi: 3435
+title: "Deneme Sınavı"
+updated_at: "2020-10-30T12:00:55.103Z"
+*/
 
   return (
     
     <div className="col-sm-12 shadow">
-      <span className="badge badge-secondary">Exam: {tmp} </span>
-      <div className="exam ">
-        <p className="examText">
-        {exams.title ? exams.title : ''}
-        </p>
-        <form>
-          
-        </form>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-8">
-          <Button variant="secondary" size="md" onClick={previous} disabled={ (!(exams.id-1))}>
-            Önceki Sınav
-          </Button>{' '}
-          <Button variant="success" size="md" onClick={next}  disabled={  ((exams.id>15))}>
-            Sonraki Sınav
-          </Button>{// next/previous/tamamla cevaplari alacak ve backende submit edecek.
-          }
-        </div>
-        <div className="col-6 col-md-4">
-          <Button variant="outline-primary" size="md" onClick={submit}  >
-            Sınavı Al
-          </Button>
-        </div>
-      </div>
+      {exams}
     </div>
   );
 }export default Exams;
