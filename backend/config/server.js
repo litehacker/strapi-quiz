87d8cf9786c152c9d551a30fba8f498614f11e6d
Giorgi Gvimradze <natceo@gmail.com>
2020-11-16 00:44:01 +0400

    backend config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://sinavhukuk.com/api',
   admin: {
+    url: 'https://sinavhukuk.com/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7239e56bb2e4911a109fe31cbff59198'),
     },
